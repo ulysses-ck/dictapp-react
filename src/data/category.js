cd639aa4c3244a18c023a255a60d42be3a8a37fd
ulysses-ck <50756389+ulysses-ck@users.noreply.github.com>
2022-08-20 20:00:43 -0300

    Fix: extra languages options
    API just give support rigth now for english language and anymore

--- src/data/category.js
+++ src/data/category.js
@@ -3,46 +3,6 @@ const categories = [
         value: "English",
         label: "en",
     },
-    {
-        value: "Hindi",
-        label: "hi",
-    },
-    {
-        value: "Spanish",
-        label: "es",
-    },
-    {
-        value: "French",
-        label: "fr",
-    },
-    {
-        value: "Japanese",
-        label: "ja",
-    },
-    {
-        value: "Russian",
-        label: "ru",
-    },
-    {
-        value: "German",
-        label: "de",
-    },
-    {
-        value: "Italian",
-        label: "it",
-    },
-    {
-        value: "Korean",
-        label: "ko",
-    },
-    {
-        value: "Arabic",
-        label: "ar",
-    },
-    {
-        value: "Turkish",
-        label: "tr",
-    },
 ];
 
 export default categories;
